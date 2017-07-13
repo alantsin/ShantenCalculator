e73f4f38fa91388c61e3123454dc0a1cc62f8e4d
Alan Tsin <alantsin92@gmail.com>
2017-07-13 09:36:05 -0700

    first commit

---
+++ ShantenCalculator/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShantenCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creates an initial hand, to be replaced with actual hand creation function
+            Tile[] hand = Make9Gates();
+
+            Console.WriteLine("Showing initial hand:");
+
+            // Prints to console the initial hand
+            for (int i = 0; i < 13; i++)
+            {
+                Console.Write(hand[i].Print());
+            }
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to draw...");
+
+            Console.ReadKey();
+
+            // Draws a tile
+            hand[13] = DrawTile();
+
+            Console.WriteLine("Showing new hand:");
+
+            for (int i = 0; i < 14; i++)
+            {
+                Console.Write(hand[i].Print());
+            }
+
+            Console.WriteLine("");
+
+            // Checks for Shanten, to be replaced
+            CheckSame(hand[0], hand[13]);
+            Console.WriteLine("Tsumo! 9 Gates, 48,000 all, 16,000 each");
+
+        }
+
+        // Makes a starting hand of 9-way wait of 9 gates and returns that hand, for testing purposes
+        public static Tile[] Make9Gates()
+        {
+            Tile[] hand = new Tile[14];
+
+            hand[0] = new Tile(1, TileType.MAN);
+
+            hand[1] = new Tile(1, TileType.MAN);
+
+            hand[2] = new Tile(1, TileType.MAN);
+
+            hand[3] = new Tile(2, TileType.MAN);
+
+            hand[4] = new Tile(3, TileType.MAN);
+
+            hand[5] = new Tile(4, TileType.MAN);
+
+            hand[6] = new Tile(5, TileType.MAN);
+
+            hand[7] = new Tile(6, TileType.MAN);
+
+            hand[8] = new Tile(7, TileType.MAN);
+
+            hand[9] = new Tile(8, TileType.MAN);
+
+            hand[10] = new Tile(9, TileType.MAN);
+
+            hand[11] = new Tile(9, TileType.MAN);
+
+            hand[12] = new Tile(9, TileType.MAN);
+
+            return hand;
+        }
+
+        // Draws a tile from the wall, for now the tile is manually coded in and not random
+        public static Tile DrawTile()
+        {
+            Tile tile = new Tile(9, TileType.MAN);
+            return tile;
+        }
+
+        // Checks the entire hand, returning an array of tile pairs
+        public static Tile[][] CheckPairs(Tile[] hand)
+        {
+            Tile[][] eyes = new Tile[7][];
+            return eyes;
+        }
+
+        // Check if two tiles are the same
+        public static Boolean CheckSame(Tile tile1, Tile tile2)
+        {
+            if (tile1.value.Equals(tile2.value) && (tile1.type.Equals(tile2.type)))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ShantenCalculator/Tile.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShantenCalculator
+{
+    // Identifier for suit type of tile
+    public enum TileType { MAN, SOU, PIN, HON }
+    class Tile
+    {
+        // The value of the tile, from 1-9, or 1-7 for honor tiles, where 1-4 are East, South, West, North, and 5-7 are Haku, Hatsu, Chun
+        public int value { get; }
+        public TileType type { get; }
+        // If a tile is a red Dora or not, either instantiated at the beginning of tile creation, or the tile became red by other means
+        public Boolean red { get; set; }
+        // If a tile is a component of Ryuuiisou
+        public Boolean green { get; }
+
+
+        public Tile(int value, TileType type)
+        {
+            this.value = value;
+
+            this.type = type;
+        }
+
+        public String Print()
+        {
+            String t;
+
+            switch (type)
+            {
+                case TileType.MAN:
+                    t = "M";
+                    break;
+                case TileType.SOU:
+                    t = "S";
+                    break;
+                case TileType.PIN:
+                    t = "P";
+                    break;
+                default:
+                    t = "H";
+                    break;
+            }
+
+            return "[" + value + t + "] ";
+        }
+    }
+}
