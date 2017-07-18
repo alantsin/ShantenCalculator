c62f896c542b27d09bb41dd588bbea7f50c391ad
Alan Tsin <alantsin92@gmail.com>
2017-07-18 09:14:12 -0700

    First part of finding all eyes and removing duplicates done

--- ShantenCalculator/Program.cs
+++ ShantenCalculator/Program.cs
@@ -10,6 +10,8 @@ namespace ShantenCalculator
     {
         static void Main(string[] args)
         {
+            int shantenCount;
+
             // Creates an initial hand
             HandGenerator handGenerator = new HandGenerator();
 
@@ -49,15 +51,58 @@ namespace ShantenCalculator
             int numPairs = pairList.Count;
             Console.WriteLine(numPairs);
 
+            // With the pairs list, check for all Pon and Chi
+            List<Tile> tempHand = CheckMelds(pairList, hand);
+
             // Finish with point calculation
             Console.WriteLine("Tsumo! 9 Gates, 48,000 all, 16,000 each");
             
         }
 
+        public static List<Tile> CheckMelds(List<int[]> pairList, Tile[] hand)
+        {
+            // This list holds all melds
+            List<Tile> tempHand = new List<Tile>();
+
+            // Check for Pon
+            pairList.ForEach(delegate (int[] eyes) {
+
+                int eye1 = eyes[0];
+                int eye2 = eyes[1];
+
+                for (int i = 0; i < hand.Length; i++)
+                {
+                    if (i != eye1 || i != eye2)
+                    {
+                        for (int j = i + 1; j < hand.Length; j++)
+                        {
+                            if (hand[i].value.Equals(hand[j].value) && hand[i].type.Equals(hand[j].type))
+                            {
+                                for (int k = j + 1; k < hand.Length; k++)
+                                {
+                                    if (hand[j].value.Equals(hand[k].value) && hand[j].type.Equals(hand[k].type))
+                                    {
+                                        tempHand.Add(hand[i]);
+                                        tempHand.Add(hand[j]);
+                                        tempHand.Add(hand[k]);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+
+            });
+
+            // Check for Chi
+
+            return tempHand;
+        }
+
         // Draws a tile from the wall, for now the tile is manually coded in and not random
         public static Tile DrawTile()
         {
-            Tile tile = new Tile(7, TileType.HON);
+            Tile tile = new Tile(9, TileType.MAN);
             return tile;
         }
 
@@ -65,6 +110,8 @@ namespace ShantenCalculator
         public static List<int[]> CheckPairs(Tile[] hand)
         {
             List<int[]> pairList = new List<int[]>();
+
+            // Generate list of eye combinations
  
             for (int i = 0; i < hand.Length; i++)
             {
@@ -76,6 +123,29 @@ namespace ShantenCalculator
                     }
         
                 }
+            }
+
+            // Remove duplicates
+            List<int> tempIndex = new List<int>() ;
+            for (int i = 0; i < pairList.Count; i++)
+            {
+                for (int j = i+1; j < pairList.Count; j++)
+                {
+                    if (hand[pairList.ElementAt(i)[0]].value.Equals(hand[pairList.ElementAt(j)[0]].value) &&
+                        hand[pairList.ElementAt(i)[0]].type.Equals(hand[pairList.ElementAt(j)[0]].type))
+                    {
+                        if (!tempIndex.Contains(i))
+                        {
+                            tempIndex.Add(i);
+                        }
+                        
+                    }
+                }
+            }
+
+            for (int i = tempIndex.Count; i > 0; i--)
+            {
+                pairList.RemoveAt(i);
             }
 
             return pairList;
--- ShantenCalculator/Tile.cs
+++ ShantenCalculator/Tile.cs
@@ -13,6 +13,8 @@ namespace ShantenCalculator
         // The value of the tile, from 1-9, or 1-7 for honor tiles, where 1-4 are East, South, West, North, and 5-7 are Haku, Hatsu, Chun
         public int value { get; }
         public TileType type { get; }
+        // If a tile is called to make a meld, then that tile and the other ones that are part of the meld become open
+        public Boolean open { get; set; }
         // If a tile is a red Dora or not, either instantiated at the beginning of tile creation, or the tile became red by other means
         public Boolean red { get; set; }
         // If a tile is a component of Ryuuiisou
@@ -24,6 +26,32 @@ namespace ShantenCalculator
             this.value = value;
 
             this.type = type;
+
+            this.open = false;
+
+            this.green = isGreen(value, type);
+        }
+
+        public Boolean isGreen(int value, TileType type)
+        {
+            if (type.Equals(TileType.SOU))
+            {
+                if (value == 2 || value == 3 || value == 4 || value == 6 || value == 8)
+                {
+                    return true;
+                }
+
+            }
+
+            else if (type.Equals(TileType.HON))
+            {
+                if (value == 6)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         public String Print()
